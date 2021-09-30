798dcf1191570ae4ea6739e46735c74949233d94
Bastian Perez <didier.bpp@gmail.com>
2021-09-30 18:24:17 -0300

    Comienzo interfaz cargad documentos

--- src/components/commons/PreviewDocument.tsx
+++ src/components/commons/PreviewDocument.tsx
@@ -1,36 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React from "react"
 
 interface PropsPreviewDocument{
     file:File,
 
 }
+{/* <div className="commons__preview-thumbail-container">
+<span className="commons__preivew-thumbail-pdf-hidde-borders">
 
-export const PreviewDocument = ({file}:PropsPreviewDocument) => {
-    const [pathFile, setPathFile] = useState<string | ArrayBuffer |null >(null)
-    const getPath = () =>{
-        const reader = new FileReader()
-        reader.readAsDataURL(file)
-        reader.onload = () =>{  
-            setPathFile(reader.result)
+</span>
+<iframe src={pathUrl+"#toolbar=1&zoom=-150"} className="commons__preview-thumbail-pdf" allowTransparency allowFullScreen    >
+    <a href={pathUrl} id="enlaceDescargarPdf" download={pathUrl}>
+        Tu dispositivo no puede visualizar los PDF, da click aquí para descargarlo
+    </a>
+</iframe  > 
 
-        }
-        reader.onerror = (error) =>{
-            console.log("ERROR",error)
-        }
-        console.log(pathFile)
-    }
-    useEffect(() => {
-        getPath()
-    }, [file])
+
+
+
+</div> */}
+export const PreviewDocument = React.memo(({file}:PropsPreviewDocument) => {
+    const pathUrl = URL.createObjectURL(file)
+    console.log(pathUrl)
+
     return (
-        <div style={{ width: '100%', height: '900px' }}>
-        
-            <iframe src={""} width="100%" height="800px">
-                <a href={""} id="enlaceDescargarPdf"
-                    download={file.name}
-                >Tu dispositivo no puede visualizar los PDF, da click aquí para descargarlo</a>
-            </iframe>
+        <div className="commons__preview-thumbail-container">
+            <span className="commons__preivew-thumbail-pdf-hidde-borders">
+
+            </span>
+            <iframe src={pathUrl+"#toolbar=1&zoom=-150"} className="commons__preview-thumbail-pdf" allowTransparency allowFullScreen    >
+                <a href={pathUrl} id="enlaceDescargarPdf" download={pathUrl}>
+                    Tu dispositivo no puede visualizar los PDF, da click aquí para descargarlo
+                </a>
+            </iframe  > 
+           
+         
+            {/* <img  src={pathUrl.replace("blob:","")}  style={{transform:"scale(1.6)"}} width="100px" height="100px"/> */}
+      
          
         </div>
     )
-}
+})
--- src/views/admin/UploadDocument/components/UploadDocument.tsx
+++ src/views/admin/UploadDocument/components/UploadDocument.tsx
@@ -1,6 +1,6 @@
-import { faFileUpload } from '@fortawesome/free-solid-svg-icons'
+import { faEye, faFilePdf, faFileUpload, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { CircularProgress } from '@material-ui/core'
+import { CircularProgress, Tooltip } from '@material-ui/core'
 import { useDragAndDrop } from '../../../../hooks/useDragAndDrop'
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../../state/reducers/index';
@@ -9,35 +9,49 @@ import { PreviewDocument } from '../../../../components/commons/PreviewDocument'
 
 export const UploadDocument = () => {
     const { loading } =  useSelector((state:RootState):IUiState => state.ui)
-    const {getRootProps, getInputProps,isDragActive,isDragReject,acceptedFiles,myFiles,setMyFiles} = useDragAndDrop()
+    const {getRootProps, getInputProps,isDragActive,isDragReject,acceptedFiles,myFiles} = useDragAndDrop()
   
     return (
+        <>
+        
         <div className="upload-documents__container-dropzone">
             {/* <h1>Subir archivo</h1> */}
+            {/* TODO: CAMBIAR INTERFAZ */}
             <div {...getRootProps()} className="upload-documents__dragdrop-zone">
                             <input  disabled={loading} name="document"  {...getInputProps()}  />
                             { loading && <CircularProgress />}
-                            { acceptedFiles.length !== 1  && loading !== true &&  <FontAwesomeIcon icon={faFileUpload} />}
+                            { loading === false &&  acceptedFiles.length ===1 && <p>Cambiar archivo</p>}
+                            { acceptedFiles.length !== 1  && loading !== true &&  <FontAwesomeIcon className="upload-documents__icons-files" icon={faFileUpload} />}
                             {!isDragActive && loading !== true && acceptedFiles.length !== 1  && <p>Arrastra y deja aqui, o haga clic para seleccionar</p>}
                             {isDragActive && loading !== true &&  !isDragReject &&  <p>Deja archivos aquí</p>}
                             { loading !== true &&  isDragReject && <p>Tipo de archivo no permitido</p>}
-                            
 
-                            <div>
+            </div>
                                 {
-                                    myFiles.map((file:any,index:number) => (
-                                        <div key={index}>
-                                            <div>
-                                                <FontAwesomeIcon icon={faFileUpload} />
-                                                <h2>{file.name}</h2>
+                                    myFiles.map((file,index:number) => (
+                                      
+                                            <div key={index} className="upload-documents__container-file-charged">
+                                                <div className="upload-documents__file-charged-name">
+                                                    <p>{file.name}</p>
+                                                </div>
+                                                <div  className="upload-documents__file-charged-item">
+                                                    <FontAwesomeIcon size="1x"  icon={faEye} />
+                                                </div>
+                                                <div  className="upload-documents__file-charged-item">
+                                                    <FontAwesomeIcon size="1x" icon={faTrash} />
+                                                </div>
+                                                
+                                            
                                             </div>
-                                        </div>
+                                      
+                                    
                                     ))
                                 }
-                            </div>
+                           
 
-                        </div>
-                     { myFiles.length &&  <PreviewDocument file={myFiles[0]}  />}
+          
         </div>
+        { !!myFiles.length &&  <PreviewDocument file={myFiles[0]}  />}
+        </>
     )
 }
